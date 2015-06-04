20673febc49644c7d2f18a70d1c80eae6718218a
René Samselnig <Rene.Samselnig@skiline.cc>
2015-06-04 15:52:53 +0200

    Fixes failing MapRendererTest test.

--- test/junit/net/yura/domination/mapstore/MapRendererTest.java
+++ test/junit/net/yura/domination/mapstore/MapRendererTest.java
@@ -32,21 +32,23 @@ public class MapRendererTest extends TestCase {
         String test3 = "hello string\nbob the builder\n";
         String test4 = "hello string\nbob\nthe\nbuilder";
         String test5 = "hello string\n\n\nbob\na";
+        String expected1 = "hello string";
+        String expected2 = "hello string...";
 
-        assertEquals(test1,MapRenderer.getFirstLines(test1, 1));
-        assertEquals(test1,MapRenderer.getFirstLines(test2, 1));
-        assertEquals(test1,MapRenderer.getFirstLines(test3, 1));
-        assertEquals(test1,MapRenderer.getFirstLines(test4, 1));
-        assertEquals(test1,MapRenderer.getFirstLines(test5, 1));
+        assertEquals(expected1,MapRenderer.getFirstLines(test1, 1));
+        assertEquals(expected2,MapRenderer.getFirstLines(test2, 1));
+        assertEquals(expected2,MapRenderer.getFirstLines(test3, 1));
+        assertEquals(expected2,MapRenderer.getFirstLines(test4, 1));
+        assertEquals(expected2,MapRenderer.getFirstLines(test5, 1));
 
-        assertEquals(test1,MapRenderer.getFirstLines(test2, 1));
+        assertEquals(expected2,MapRenderer.getFirstLines(test2, 1));
         assertEquals(test2,MapRenderer.getFirstLines(test2, 2));
         assertEquals(test2,MapRenderer.getFirstLines(test2, 3));
         assertEquals(test2,MapRenderer.getFirstLines(test2, 4));
         assertEquals(test2,MapRenderer.getFirstLines(test2, 5));
         
-        assertEquals(test1,MapRenderer.getFirstLines(test3, 1));
-        assertEquals(test2,MapRenderer.getFirstLines(test3, 2));
+        assertEquals(expected2,MapRenderer.getFirstLines(test3, 1));
+        assertEquals(test2 + "...",MapRenderer.getFirstLines(test3, 2));
         assertEquals(test3,MapRenderer.getFirstLines(test3, 3));
         assertEquals(test3,MapRenderer.getFirstLines(test3, 4));
         assertEquals(test3,MapRenderer.getFirstLines(test3, 5));
